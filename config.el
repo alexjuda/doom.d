9239674db5518ad700e8b0c6caeeecf4dd074f6f
Alexander Juda <alexander.juda@lekta.ai>
2020-10-05 16:40:12 +0200

    Open current buffer in neotree

--- config.el
+++ config.el
@@ -54,13 +54,37 @@
 ;; they are implemented.
 
 
-;; My config
+;; ------ My config ------
+
+;; ------ Custom interactive fns ------
+
+;; Open current buffer in neotree
+;; source: https://github.com/jaypei/emacs-neotree/issues/149
+(defun aj/neotree-project-root-dir-or-current-dir ()
+  "Open NeoTree using the project root, using projectile, or the
+current buffer directory."
+  (interactive)
+  (let ((project-dir (ignore-errors (projectile-project-root)))
+        (file-name (buffer-file-name))
+        (neo-smart-open t))
+    (if (neo-global--window-exists-p)
+        (neotree-hide)
+      (progn
+        (neotree-show)
+        (if project-dir
+            (neotree-dir project-dir))
+        (if file-name
+            (neotree-find file-name))))))
 
 
-;; Keybindings
+;; ------ Keybindings ------
+
+;; (map! (:leader
+;;        (:prefix "p"
+;;         :desc "Toggle neotree" "t" #'+neotree/open)))
 (map! (:leader
        (:prefix "p"
-        :desc "Toggle neotree" "t" #'+neotree/open)))
+        :desc "Toggle open buffer in neotree" "t" #'aj/neotree-project-root-dir-or-current-dir)))
 
 (map! (:leader
        (:prefix "b"
@@ -106,3 +130,4 @@
   (doom/set-indent-width 2))
 
 (add-hook 'json-mode-hook #'aj-set-indent-width-2)
+
